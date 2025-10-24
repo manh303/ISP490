04b88048e92393b43feedc55844662c1c8b9bf7b
HaiMinh2003 <phuchaiminh03@gmail.com>
2025-10-25 04:27:08 +0700

    add back to home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import HeroSection from '../components/home/HeroSection';
-import FeaturesSection from '../components/home/FeaturesSection';
-import FeatureCard from '../components/home/FeatureCard';
+// import HeroSection from '../components/home/HeroSection';
+// import FeaturesSection from '../components/home/FeaturesSection';
+// import FeatureCard from '../components/home/FeatureCard';
 
 const Home: React.FC = () => {
   return (
     <div className="home-page" style={{background: '#fff', minHeight: '100vh'}}>
-      <HeroSection />
+      {/* <HeroSection />
       <div style={{maxWidth: '1200px', margin: '0 auto', padding: '60px 20px'}}>
         <div style={{textAlign: 'center', marginBottom: '48px'}}>
           <h2 style={{
@@ -58,7 +58,7 @@ const Home: React.FC = () => {
             icon={<svg width="24" height="24" viewBox="0 0 24 24" fill="#00cc00"><path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.07.63-.07.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z"/></svg>}
           />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
