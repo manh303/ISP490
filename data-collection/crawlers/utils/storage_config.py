27396ae3273976e6f85de931160fc16092fc18d8
manh303 <manh027382@gmail.com>
2025-11-14 05:27:25 +0700

    update env

--- data-collection/crawlers/utils/storage_config.py
+++ data-collection/crawlers/utils/storage_config.py
@@ -5,8 +5,8 @@ STORAGE_TYPE = os.getenv("STORAGE_TYPE", "minio")  # minio or s3
 if STORAGE_TYPE == "s3":
     # Production: AWS S3
     ENDPOINT = None  # Use default AWS endpoint
-    ACCESS_KEY = os.getenv("AKIAYZLPH4I7BTUCL6YS")
-    SECRET_KEY = os.getenv("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")
+    ACCESS_KEY = os.getenv("minoadmin")
+    SECRET_KEY = os.getenv("minioadmin123")
     BUCKET = "ecommerce-crawler-prod"
     SECURE = True
 else:
